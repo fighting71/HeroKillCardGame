0f2fe092ee4e4f834994afa949014f833efd920a
fighting71 <35062179+fighting71@users.noreply.github.com>
2021-06-28 11:20:17 +0800

    async

---
+++ src/Card.HeroKill/Test/ConsoleApp/Defind/PlayDefind.cs
@@ -0,0 +1,51 @@
+﻿using Card.HeroKill.Core.Menu;
+using Card.HeroKill.Data.Model;
+using Card.HeroKill.Infrastructure.CardGroup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Defind
+{
+    /// <summary>
+    /// @auth : monster
+    /// @since : 6/28/2021 11:07:25 AM
+    /// @source : 
+    /// @des : 
+    /// </summary>
+    public abstract class PlayDefind
+    {
+
+        public void Run()
+        {
+
+            while (true)
+            {
+
+                Choice choice = GetBestChoice();
+
+                if (choice == null) return;
+
+                Use(choice.Card);
+
+            }
+
+        }
+
+        public abstract bool HaveChoice();
+
+        public abstract Choice GetBestChoice();
+
+        public abstract bool Use(CardInfo card);
+
+    }
+
+    public class Choice
+    {
+        public CardInfo Card { get; set; }
+        public Gamer Target { get; set; }
+    }
+
+}
